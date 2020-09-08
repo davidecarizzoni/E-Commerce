822a5c0044e1e0d74c739f47973ec1e1946c51b3
davidecarizzoni <davidecarizzoni@gmail.com>
2020-09-08 16:04:04 +0200

    Add logout method

--- src/app/features/home/components/main/home.component.ts
+++ src/app/features/home/components/main/home.component.ts
@@ -3,6 +3,7 @@ import { Store, select } from '@ngrx/store';
 import { Component, OnInit } from '@angular/core';
 import { map, filter } from 'rxjs/operators';
 import { getCurrentUser } from 'src/app/redux/users';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -23,7 +24,12 @@ export class HomeComponent implements OnInit {
     );
   }
 
-  constructor(private store: Store) {}
+  constructor(private store: Store, private router: Router) {}
+
+  logout(){
+    sessionStorage.setItem("user", '');
+    this.router.navigateByUrl('/login');
+  }
 
   ngOnInit(): void {}
 
