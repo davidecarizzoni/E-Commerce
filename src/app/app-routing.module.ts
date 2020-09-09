397734108471838c39ef79d4bd041f9f760c7946
davidecarizzoni <davidecarizzoni@gmail.com>
2020-09-09 11:31:04 +0200

    Creazione customise components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { LoginGuardService } from './core/guards/login-guard.service';
 const routes: Routes = [
   { path: 'login', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule) },
   { path: 'signup', loadChildren: () => import('./features/signup/signup.module').then(m => m.SignupModule) },
+  { path: 'customize', loadChildren: () => import('./features/customize/customize.module').then(m => m.CustomizeModule) },
   { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule), canLoad: [LoginGuardService] },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
