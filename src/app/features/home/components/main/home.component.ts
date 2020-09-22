05ba9b364b739b73c35b42b75f77967173458b12
davidecarizzoni <davidecarizzoni@gmail.com>
2020-09-22 16:29:49 +0200

    fix home bug

--- src/app/features/home/components/main/home.component.ts
+++ src/app/features/home/components/main/home.component.ts
@@ -1,12 +1,7 @@
-import { Observable } from 'rxjs';
-import { Store, select } from '@ngrx/store';
+import { Store} from '@ngrx/store';
 import { Component, OnInit } from '@angular/core';
-import { map, filter } from 'rxjs/operators';
-import { getCurrentUser } from 'src/app/redux/users';
 import { Router } from '@angular/router';
-import { retrieveAllUsers } from 'src/app/redux/users/users.action';
-import { CartItem } from 'src/app/core/model/cart-item.interface';
-import { selectClothes } from 'src/app/redux/clothes';
+
 
 
 @Component({
@@ -19,22 +14,15 @@ export class HomeComponent implements OnInit {
   id: number;
 
   constructor(private store: Store, private router: Router) {
-    this.id=1;
-  }
-
-  //return clothes using NgRx store
-  get clothes(): Observable<CartItem[]> {
-    return this.store.pipe(select(selectClothes));
+    this.id = 0;
   }
 
   currentClothesId(n:number){
     this.id = n;
-    console.log(this.id);
   }
 
   goToCustomize(){
-    console.log("Cutomize pressed -> " + this.id);
-    this.router.navigate(['/customize', this.id-1]);
+    this.router.navigate(['/customize', this.id]);
   }
 
   ngOnInit(): void {
