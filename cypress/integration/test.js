d05f8091e8a04f04e3f355927e1b4951cf669275
davidecarizzoni <davidecarizzoni@gmail.com>
2020-09-23 09:47:43 +0200

    Finishh

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -11,3 +11,19 @@ describe('Visit the home page', () => {
       cy.visit("http://localhost:4200/home")
     })
 })
+
+describe('Login', () => {
+  it('Visit the Login page', () => {
+      cy.visit('http://localhost:4200/login')
+      cy.get('input[id=username]')
+          .type('davidecarizzoni')
+      cy.get('input[id=password]')
+          .type('1234')
+
+      cy.get('button[id=button]')
+          .click()
+
+      cy.url('/home')
+      cy.url().should('include', '/home')
+  })
+})
